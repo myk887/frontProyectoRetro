4b33c98f09a53a926bffab8654900067cfe8e977
mykell887 <mykell887@gmail.com>
2022-02-21 08:18:55 +0100

    activity

--- src/MyActivity/ArticlesBuyer.js
+++ src/MyActivity/ArticlesBuyer.js
@@ -1,23 +1,16 @@
 import { useNavigate } from 'react-router-dom'
-import React, { Suspense, useCallback, useEffect, useState } from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 import './MyActivity.css'
 import { useSelector } from 'react-redux'
 import  useFetch  from "fetch-suspense"
 import Loading from '../Loading'
 
-const fetchFun = async ({id, opts}) => {
-  let res = await fetch('http://localhost:3000/articles/' + id, opts)
-  let data = await res.json()
-  return data
-}
 
 
 function ArticlesBuyer(){
 
-  const datos = useSelector(d => d.userDatos)
   const userToken = useSelector(s => s.user)
   const navigate = useNavigate()
-  const [array, setArry] = useState([])
   const [fecha, setFecha] = useState('')
   const [loading, setLoading] = useState(false)
   useEffect(() => {
@@ -34,44 +27,64 @@ function ArticlesBuyer(){
   datosArticulos = useFetch('http://localhost:3000/trading/userSeller', opts)
   if (!userToken?.token) datosArticulos = undefined
 
-  const misArticlesPromises = datosArticulos?.map(async (element, i) => {
-    const article = await fetchFun({ id: element.idArtilce, opts})
-
-    return article
-  });
-  let oa = []
-  const id = misArticlesPromises?.map(async element => {
-    const id = await element.then(v => v)
-    oa.push(id)
-    return oa
-  })
-  datosArticulos.length && !array.length && id[misArticlesPromises.length-1].then(v => setArry(v))
-  useCallback(() => id[misArticlesPromises.length-1].then(v => setArry(v)), [id, setArry, misArticlesPromises])
 
   const handleSubmit = async (e, {idArticle, idBuyer, location, province}) => {
     setLoading(true)
     e.preventDefault()
-    const fechaHora = fecha.split('Z')[0].split('T')
-    console.log(fecha, fechaHora)
-    const accept = {
+    const date = new Date(fecha)
+    const date2 = new Date()
+    if (date > date2) {
+      const fechaHora = fecha.split('Z')[0].split('T')
+      const accept = {
+        idBuyer: idBuyer,
+        articleId: idArticle,
+        tradingInfo : {
+            buy : 1,
+            salePlace: location + ' ' + province,
+            saleDate: fechaHora[0],
+            saleHour: fechaHora[1] + ':00'
+        }
+      }
+      const res = await fetch('http://localhost:3000/trading/userSeller', {
+        method: 'POST',
+        body: JSON.stringify(accept),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + userToken?.token
+        }
+      })
+      if (res.ok) {
+        window.location.reload(true);
+        setLoading(false)
+      } else {
+        console.log(res)
+        setLoading(false)
+      }} else {
+        alert('fecha incorrecta')
+        setLoading(false)
+      }
+  }
+  const handleSubmit2 = async (e, {idArticle, idBuyer, location, province}) => {
+    setLoading(true)
+    e.preventDefault()
+    const noAccept = {
       idBuyer: idBuyer,
       articleId: idArticle,
       tradingInfo : {
-          buy : 1,
+          buy : 0,
           salePlace: location + ' ' + province,
-          saleDate: fechaHora[0],
-          saleHour: fechaHora[1] + ':00'
       }
   }
     const res = await fetch('http://localhost:3000/trading/userSeller', {
       method: 'POST',
-      body: JSON.stringify(accept),
+      body: JSON.stringify(noAccept),
       headers: {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer ' + userToken?.token
       }
     })
     if (res.ok) {
+      window.location.reload(true);
       setLoading(false)
     } else {
       console.log(res)
@@ -84,10 +97,10 @@ function ArticlesBuyer(){
     <div className="datauser"><Loading /></div> :
     <div className='soldBuy'>
       {
-        !array.length ?
+        !datosArticulos[0].length ?
         <span>NO TIENES PRODUCTOS COMPRADOS</span>
         :
-        array.map((articulo, i) =>
+        datosArticulos[0]?.map((articulo, i) =>
           <div key={articulo.id} className="comercio">
             <div  className="MyArticle">
                 <img className="image" src={`http://localhost:3000${articulo.photo.replace('./', '/')}`} alt={articulo.name} />
@@ -99,16 +112,20 @@ function ArticlesBuyer(){
                 </div>
             </div>
             <div className='estado'>
-              {(datosArticulos[i].buy === 1) && articulo.buyerId && <div> <h2>Estado de la venta</h2> <span>✅</span> </div>}
-              {(datosArticulos[i].buy === 1) && !articulo.buyerId && <div><h2>Día de compra</h2> <p>{datosArticulos[i].saleDate.split('Z')[0].split('T')[0]}{' '}{datosArticulos[i].saleDate.split('Z')[0].split('T')[1].slice(0, -4)}</p> </div>}
-              {(datosArticulos[i].buy === 1) && !articulo.buyerId && datosArticulos[i].saleDate > new Date() && console.log('hola')}
-              {(datosArticulos[i].buy === 0) && <span>❌</span>}
-              {(datosArticulos[i].buy === null) && <div>
+              {(datosArticulos[1][i].buy === 1) && articulo.buyerId && <div> <h2>Estado de la venta</h2> <span>✅</span> </div>}
+              {(datosArticulos[1][i].buy === 1) && !articulo.buyerId && <div><h2>Día de compra</h2> <p>{datosArticulos[1][i].saleDate.split('Z')[0].split('T')[0]}{' '}{datosArticulos[1][i].saleDate.split('Z')[0].split('T')[1].slice(0, -4)}</p> </div>}
+              {(datosArticulos[1][i].buy === 0) && <span>❌</span>}
+              {(datosArticulos[1][i].buy === null) && <div>
                   <h2>Estado de la venta</h2>
-                  <form onSubmit={(e)=> handleSubmit(e, {idArticle: articulo.id, idBuyer: datosArticulos[i].buyerId, location: articulo.location, province: articulo.province})}>
+                  <form onSubmit={(e)=> handleSubmit(e, {idArticle: articulo.id, idBuyer: datosArticulos[1][i].buyerId, location: articulo.location, province: articulo.province})}>
                     <label>
                       <input required type="datetime-local" name="horareunion" onChange={(e)=> setFecha(e.target.value)} />
-                      <button>poner horareunion</button>
+                      <button>Poner hora</button>
+                    </label>
+                  </form>
+                  <form onSubmit={(e)=> handleSubmit2(e, {idArticle: articulo.id, idBuyer: datosArticulos[1][i].buyerId, location: articulo.location, province: articulo.province})}>
+                    <label>
+                      <button>No quiero vender</button>
                     </label>
                   </form>
                 </div>}
@@ -125,8 +142,3 @@ const ArticlesBuyerWrapper = () =>
   </Suspense>
 
 export default ArticlesBuyerWrapper
-
-/* {(datosArticulos.buy === 1) && <span>✅</span>}
-              {(datosArticulos.buy === 1) && <p>{datosArticulos.saleDate}</p>}
-              {(datosArticulos.buy === 0) && <span>❌</span>}
-              {(datosArticulos.buy === null) && <span>Pendiente de respuesta</span>} */
--- src/MyActivity/ArticlesSold.js
+++ src/MyActivity/ArticlesSold.js
@@ -1,23 +1,14 @@
-import { Link, useNavigate } from 'react-router-dom'
-import React, { Suspense, useCallback, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import React, { Suspense, useEffect } from 'react'
 import './MyActivity.css'
 import { useSelector } from 'react-redux'
 import  useFetch  from "fetch-suspense"
 import Loading from '../Loading'
 
-const fetchFun = async ({id, opts}) => {
-  let res = await fetch('http://localhost:3000/articles/' + id, opts)
-  let data = await res.json()
-  return data
-}
-
 
 function ArticlesSold() {
-
-  const datos = useSelector(d => d.userDatos)
   const userToken = useSelector(s => s.user)
   const navigate = useNavigate()
-  const [array, setArry] = useState([])
 
   useEffect(() => {
     !userToken && navigate('user/registre')
@@ -32,28 +23,30 @@ function ArticlesSold() {
   datosArticulos = useFetch('http://localhost:3000/trading/userBuyer', opts)
   if (!userToken?.token) datosArticulos = undefined
 
-  const misArticlesPromises = datosArticulos?.map(async (element, i) => {
-    const article = await fetchFun({ id: element.idArtilce, opts})
-
-    return article
-  });
-  let oa = []
-  const id = misArticlesPromises?.map(async element => {
-    const id = await element.then(v => v)
-    oa.push(id)
-    return oa
-  })
-  datosArticulos.length && !array.length && id[misArticlesPromises.length-1].then(v => setArry(v))
-  useCallback(() => id[misArticlesPromises.length-1].then(v => setArry(v)), [id, setArry, misArticlesPromises])
-
+  const handleBuy = async ({articleId}) => {
+    console.log('hola')
+    const res = await fetch('http://localhost:3000/articles/sold', {
+      method: 'POST',
+      body: JSON.stringify({articleId}),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + userToken?.token
+      }
+    })
+    if (res.ok) {
+      window.location.reload(true)
+    } else {
+      console.log(res)
+    }
+  }
 
   return (
     <div className='soldBuy'>
       {
-        !array.length ?
+        !datosArticulos[0].length ?
         <span>NO TIENES PRODUCTOS COMPRADOS</span>
         :
-        array.map((articulo, i) =>
+        datosArticulos[0]?.map((articulo, i) =>
           <div key={articulo.id} className="comercio">
             <div  className="MyArticle">
                 <img className="image" src={`http://localhost:3000${articulo.photo.replace('./', '/')}`} alt={articulo.name} />
@@ -65,10 +58,12 @@ function ArticlesSold() {
                 </div>
             </div>
             <div className='estado'>
-              {(datosArticulos[i].buy === 1) && articulo.buyerId && <div> <h2>Estado de la venta</h2> <span>✅</span> </div>}
-              {(datosArticulos[i].buy === 1) && !articulo.buyerId && <div><h2>Día de compra</h2> <p>{datosArticulos[i].saleDate?.split('Z')[0].split('T')[0]}{' '}{datosArticulos[i].saleDate?.split('Z')[0].split('T')[1].slice(0, -4)}</p> </div>}
-              {(datosArticulos[i].buy === 0) && <span>❌</span>}
-              {(datosArticulos[i].buy === null) && <div> <h2>Estado de la venta</h2> <span>Pendiente de respuesta</span> </div>}
+              {(datosArticulos[1][i].buy === 1) && articulo.buyerId && <div> <h2>Estado de la venta</h2> <span>✅</span> </div>}
+              {(datosArticulos[1][i].buy === 1) && !articulo.buyerId && <div><h2>Día de compra</h2> <p>{datosArticulos[1][i].saleDate?.split('Z')[0].split('T')[0]}{' '}{datosArticulos[1][i].saleDate?.split('Z')[0].split('T')[1].slice(0, -4)}</p> </div>}
+              {(datosArticulos[1][i].buy === 1) && !articulo.buyerId && new Date(datosArticulos[1][i].saleDate) < new Date() && handleBuy({articleId: articulo.id})}
+              {(datosArticulos[1][i].buy === 1) && articulo.buyerId && <div> <h1>Puntua</h1></div>}
+              {(datosArticulos[1][i].buy === 0) && <span>❌</span>}
+              {(datosArticulos[1][i].buy === null) && <div> <h2>Estado de la venta</h2> <span>Pendiente de respuesta</span> </div>}
             </div>
           </div>
         )}
@@ -76,36 +71,7 @@ function ArticlesSold() {
   )
 }
 
-//   let misArticulos
-//   misArticulos = useFetch('http://localhost:3000/articles/sold', opts)
-//   if (!userToken?.token) misArticulos = [undefined]
 
-//   let datosArticulos
-//   datosArticulos = useFetch('http://localhost:3000/trading//userSeller', opts)
-//   if (!userToken?.token) datosArticulos = undefined
-
-//   console.log(datosArticulos)
-
-//   return (
-//     <div className='soldBuy'>
-//       {
-//         !misArticulos.length ?
-//         <span>NO TIENES PRODUCTOS VENDIDOS</span>
-//         :
-//         misArticulos?.map(articulo =>
-//             <div key={articulo.id} className="MyArticle">
-//                 <img className="image" src={`http://localhost:3000${articulo.photo.replace('./', '/')}`} alt={articulo.name} />
-//                 <div>
-//                   <h3>{articulo.name}</h3>
-//                   <div className="textMyArticle">
-//                       <h4>{articulo.price} euros</h4>
-//                   </div>
-//                 </div>
-//             </div>
-//         )}
-//     </div>
-//   )
-// }
 
 const ArticlesBuyerWrapper = () =>
   <Suspense fallback={<Loading />}>
